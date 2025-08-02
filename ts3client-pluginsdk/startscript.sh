b94732f75923d6a1d14372933db329a9a465913b
simone <fabbrus@gmail.com>
2025-08-02 11:06:58 +0200

    test con docker

--- ts3client-pluginsdk/startscript.sh
+++ ts3client-pluginsdk/startscript.sh
@@ -1,17 +1,31 @@
 #!/bin/bash
 
-export DISPLAY=:99
-export QT_QUICK_BACKEND=software
-export TS3CLIENT_NO_CRASHDUMP=1 
-export QT_QPA_PLATFORM=xcb
+export XDG_RUNTIME_DIR=/tmp/runtime-root
+mkdir -p "$XDG_RUNTIME_DIR"
+chmod 700 "$XDG_RUNTIME_DIR"
 
-
-pulseaudio --start --exit-idle-time=-1
+# Start PulseAudio
+pulseaudio --system --exit-idle-time=-1 &
 export PULSE_SERVER=unix:/tmp/pulse.sock
-Xorg :99 -noreset +extension GLX +extension RANDR +extension RENDER -logfile /tmp/xorg.log -config /etc/X11/xorg.conf &
 
-sleep 10
+# Wait for PulseAudio socket
+for i in {1..20}; do
+  if [ -S /tmp/pulse.sock ]; then break; fi
+  echo "Waiting for PulseAudio..."
+  sleep 0.5
+done
 
-cd /TeamSpeak3-Client-linux_amd64
-./ts3client_linux_amd64 --no-sandbox --license_accepted=1 "ts3server://ts.109tech.com?port=9987&nickname=BOOTYCALL&channel=Generico"
+# Start Xorg in the background
+Xorg :99 -noreset +extension GLX +extension RANDR +extension RENDER -logfile /tmp/xorg.log -config /etc/X11/xorg.conf #&
+
+# Wait for X to be ready
+for i in {1..20}; do
+  if xdpyinfo -display :99 > /dev/null 2>&1; then break; fi
+  echo "Waiting for Xorg..."
+  sleep 0.5
+done
+
+# Launch TS3
+cd /TeamSpeak3-Client-linux_amd64/
+DISPLAY=:99 ./ts3client_linux_amd64 --no-sandbox --license_accepted=1 "ts3server://ts.109tech.com?port=9987&nickname=BOOTYCALL&channel=Generico"
 
