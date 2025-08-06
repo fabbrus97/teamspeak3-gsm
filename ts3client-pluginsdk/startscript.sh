700bb55b6d924a473ed427c057fc69406e6fdf0a
Simone Fabbri <fabbrus@gmail.com>
2025-08-06 14:49:54 +0200

    solved start problem

--- ts3client-pluginsdk/startscript.sh
+++ ts3client-pluginsdk/startscript.sh
@@ -18,24 +18,34 @@ for i in {1..20}; do
 done
 
 # Start Xorg in the background
-Xorg :99 -noreset +extension GLX +extension RANDR +extension RENDER -logfile /tmp/xorg.log -config /etc/X11/xorg.conf &
+#Xorg :99 -noreset +extension GLX +extension RANDR +extension RENDER -logfile /tmp/xorg.log -config /etc/X11/xorg.conf &
 
 # Wait for X to be ready
-for i in {1..20}; do
-  if xdpyinfo -display :99 > /dev/null 2>&1; then break; fi
-  echo "Waiting for Xorg..."
-  sleep 0.5
-done
+#for i in {1..20}; do
+#  if xdpyinfo -display :99 > /dev/null 2>&1; then break; fi
+#  echo "Waiting for Xorg..."
+#  sleep 0.5
+#done
 
 # Launch TS3
 mkdir -p /root/.ts3client
-cat ShowTS5Promotion=0 >> /root/.ts3client/settings.ini 
-cat ShowShowTS5Overview=0 >> /root/.ts3client/settings.ini 
-cat ShowDisableTS5Popup=1 >> /root/.ts3client/settings.ini 
-cat ShowShowUpgradeDialog=0 >> /root/.ts3client/settings.ini 
-cat ShowEnablePromotions=0 >> /root/.ts3client/settings.ini 
-
+#   accept the license
+# sqlite3 /root/.ts3client/settings.db "CREATE TABLE IF NOT EXISTS Misc (key TEXT PRIMARY KEY, value TEXT);"
+# sqlite3 /root/.ts3client/settings.db "INSERT OR REPLACE INTO Misc (key, value) VALUES ('LicenseAccepted', '1');"
+#   disable other dialogs
+# cat ShowTS5Promotion=0 >> /root/.ts3client/settings.ini 
+# cat ShowShowTS5Overview=0 >> /root/.ts3client/settings.ini 
+# cat ShowDisableTS5Popup=1 >> /root/.ts3client/settings.ini 
+# cat ShowShowUpgradeDialog=0 >> /root/.ts3client/settings.ini 
+# cat ShowEnablePromotions=0 >> /root/.ts3client/settings.ini 
+# cat NoAccountDialog=1 >> /root/.ts3client/settings.ini 
 cd /TeamSpeak3-Client-linux_amd64/
-#rm -rf plugins
-DISPLAY=:99 ./ts3client_linux_amd64 --no-sandbox --license_accepted=1 "ts3server://ts.109tech.com?port=9987&nickname=BOOTYCALL&channel=Generico"
+rm -rf plugins
+./ts3client_linux_amd64 --no-sandbox "ts3server://ts.109tech.com?port=9987&nickname=BOOTYCALL&channel=Generico" &
+sleep 3
+pkill ts3client_linux
+cp /app/settings.db /root/.ts3client/
+./ts3client_linux_amd64 --no-sandbox "ts3server://ts.109tech.com?port=9987&nickname=BOOTYCALL&channel=Generico"
+#DISPLAY=:99 
+
 
