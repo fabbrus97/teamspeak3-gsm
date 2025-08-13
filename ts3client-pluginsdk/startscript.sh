78b80b107eac9c737fdd729490acb568d98107bd
Simone Fabbri <fabbrus@gmail.com>
2025-08-13 14:58:58 +0200

    fixed reboot issues

--- ts3client-pluginsdk/startscript.sh
+++ ts3client-pluginsdk/startscript.sh
@@ -4,6 +4,9 @@ export XDG_RUNTIME_DIR=/tmp/runtime-root
 mkdir -p "$XDG_RUNTIME_DIR"
 chmod 700 "$XDG_RUNTIME_DIR"
 
+pkill -f pulse
+rm -rf /root/.config/pulse/*
+rm -rf /tmp/pulse-*
 # Start PulseAudio
 #pulseaudio --system --exit-idle-time=-1 &
 pulseaudio --system --exit-idle-time=-1 --disallow-exit --no-cpu-limit \
@@ -17,8 +20,11 @@ for i in {1..20}; do
   sleep 0.5
 done
 
+pkill -f Xorg
+rm -f /tmp/.X99-lock
+rm -rf /tmp/.X11-unix/X99
 # Start Xorg in the background
-Xorg :99 -noreset +extension GLX +extension RANDR +extension RENDER -logfile /tmp/xorg.log -config /etc/X11/xorg.conf &
+Xorg :99 -ac -noreset +extension GLX +extension RANDR +extension RENDER -logfile /tmp/xorg.log -config /etc/X11/xorg.conf &
 
 export DISPLAY=:99 
 
@@ -29,6 +35,7 @@ for i in {1..20}; do
   sleep 0.5
 done
 
+echo "[INFO] pulse and x OK, waiting before starting ts..."
 # Launch TS3
 sleep 90 #wait some time to not DOS the server
 mkdir -p /root/.ts3client
@@ -44,15 +51,23 @@ mkdir -p /root/.ts3client
 # cat NoAccountDialog=1 >> /root/.ts3client/settings.ini 
 cd /TeamSpeak3-Client-linux_amd64/
 # rm -rf plugins
-./ts3client_linux_amd64 --no-sandbox "ts3server://ts.109tech.com?port=9987&nickname=BOOTYCALL&channel=Generico" &
-if [ $(string /root/.ts3client/settings.db | grep -i license | wc -l) -lt 10 ]
-then 
+echo "[INFO] starting ts..."
+if ! [ -f /root/.ts3client/settings.db ]
+then
+  ./ts3client_linux_amd64 --no-sandbox "ts3server://ts.109tech.com?port=9987&nickname=BOOTYCALL&channel=Generico" &
   sleep 3
   pkill ts3client_linux
-  cp /app/settings.db /root/.ts3client/
-  ./ts3client_linux_amd64 --no-sandbox "ts3server://ts.109tech.com?port=9987&nickname=BOOTYCALL&channel=Generico"
 fi
-
-
+echo "[INFO] checking if this is the first time the docker volume is created"
+if [ $(strings /root/.ts3client/settings.db | grep -i license | wc -l) -lt 10 ]
+then 
+  # sleep 3
+  # echo "[INFO] killing TS"
+  # pkill ts3client_linux
+  echo "[INFO] installing db"
+  cp /app/settings.db /root/.ts3client/
+fi
+echo "[INFO] starting TS again"
+./ts3client_linux_amd64 --no-sandbox "ts3server://ts.109tech.com?port=9987&nickname=BOOTYCALL&channel=Generico"
 
 
