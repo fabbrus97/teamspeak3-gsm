5de2f14d7828dc57f3d419a31b85586a5d5f2c15
Simone Fabbri <fabbrus@gmail.com>
2025-09-14 09:10:05 +0200

    started developing noise canceling feature; see todo in noise_clean.c

--- ts3client-pluginsdk/src/settings.h
+++ ts3client-pluginsdk/src/settings.h
@@ -17,6 +17,10 @@ extern char* ucontroller_address; //ip of the ucontroller; defaults to 127.0.0.1
 extern char* ucontroller_cmd_port; // port of the ucontroller where it is possible to send commands; defaults to 8001
 extern char* ts_ip_bind; //ip to bind the ts plugin to; defaults to 0.0.0.0
 extern int ucontroller_audio_port; //this is the port where ucontroller will receive audio; defaults to 7000
+extern int noise_cancelnoise; //active noise canceling algorithm for audio incoming from the ucontroller? by default no
+extern char* noise_noiseprofilefile; //where to save the noise computed profile
+extern char* noise_noiserecordingfile; //raw recorded audio noise file (audio from the ucontroller)
+extern int noise_suppr_level;
 
 void load_settings();
 
