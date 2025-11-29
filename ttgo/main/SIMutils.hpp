7f64080ab3c45af135ff2fea8159bc8406e9191d
simone <fabbrus@gmail.com>
2025-11-29 14:56:41 +0100

    porting firmware to new board

---
+++ ttgo/main/SIMutils.hpp
@@ -0,0 +1,15 @@
+#ifndef SIMUTILS_H
+#define SIMUTILS_H
+#include "SETTINGS.h"
+#include <string.h>
+
+#include <HardwareSerial.h>
+
+extern int call_in_progress;
+extern int recording_noise;
+// extern SoftwareSerial GPRS;
+
+void parse_serial(const char* str);
+int check_caller_in_pb(const char* str);
+
+#endif
